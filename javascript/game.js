8adb9e2a9f810119ed3c7bda4d0ae45fcf8bcf3d
= <=>
2015-12-09 12:14:32 +0200

    Refactoring complete

--- javascript/game.js
+++ javascript/game.js
@@ -1,191 +1,222 @@
-exports = typeof window !== "undefined" && window !== null ? window : global;
+(function () {
+    var exports = (window == null) ? global : window;
 
-exports.Game = function() {
-  var players          = new Array();
-  var places           = new Array(6);
-  var purses           = new Array(6);
-  var inPenaltyBox     = new Array(6);
+    exports.Game = function () {
+        var MAX_PLAYERS = 6,
+            MIN_PLAYERS = 2,
+            WIN_GOLD = 6,
+            PLACES_COUNT = 12,
+            QUESTIONS_COUNT = 50;
 
-  var popQuestions     = new Array();
-  var scienceQuestions = new Array();
-  var sportsQuestions  = new Array();
-  var rockQuestions    = new Array();
+        var players = [],
+            places = [],
+            purses = [],
+            inPenaltyBox = [],
+            currentPlayer = 0,
+            playersCount = 0,
+            isGettingOutOfPenaltyBox = false,
+            categories = ['Pop', 'Science', 'Sports', 'Rock'],
+            questions = [];
 
-  var currentPlayer    = 0;
-  var isGettingOutOfPenaltyBox = false;
+        initQuestions();
 
-  var didPlayerWin = function(){
-    return !(purses[currentPlayer] == 6)
-  };
-
-  var currentCategory = function(){
-    if(places[currentPlayer] == 0)
-      return 'Pop';
-    if(places[currentPlayer] == 4)
-      return 'Pop';
-    if(places[currentPlayer] == 8)
-      return 'Pop';
-    if(places[currentPlayer] == 1)
-      return 'Science';
-    if(places[currentPlayer] == 5)
-      return 'Science';
-    if(places[currentPlayer] == 9)
-      return 'Science';
-    if(places[currentPlayer] == 2)
-      return 'Sports';
-    if(places[currentPlayer] == 6)
-      return 'Sports';
-    if(places[currentPlayer] == 10)
-      return 'Sports';
-    return 'Rock';
-  };
-
-  this.createRockQuestion = function(index){
-    return "Rock Question "+index;
-  };
-
-  for(var i = 0; i < 50; i++){
-    popQuestions.push("Pop Question "+i);
-    scienceQuestions.push("Science Question "+i);
-    sportsQuestions.push("Sports Question "+i);
-    rockQuestions.push(this.createRockQuestion(i));
-  };
-
-  this.isPlayable = function(howManyPlayers){
-    return howManyPlayers >= 2;
-  };
-
-  this.add = function(playerName){
-    players.push(playerName);
-    places[this.howManyPlayers() - 1] = 0;
-    purses[this.howManyPlayers() - 1] = 0;
-    inPenaltyBox[this.howManyPlayers() - 1] = false;
-
-    console.log(playerName + " was added");
-    console.log("They are player number " + players.length);
-
-    return true;
-  };
-
-  this.howManyPlayers = function(){
-    return players.length;
-  };
-
-
-  var askQuestion = function(){
-    if(currentCategory() == 'Pop')
-      console.log(popQuestions.shift());
-    if(currentCategory() == 'Science')
-      console.log(scienceQuestions.shift());
-    if(currentCategory() == 'Sports')
-      console.log(sportsQuestions.shift());
-    if(currentCategory() == 'Rock')
-      console.log(rockQuestions.shift());
-  };
-
-  this.roll = function(roll){
-    console.log(players[currentPlayer] + " is the current player");
-    console.log("They have rolled a " + roll);
-
-    if(inPenaltyBox[currentPlayer]){
-      if(roll % 2 != 0){
-        isGettingOutOfPenaltyBox = true;
-
-        console.log(players[currentPlayer] + " is getting out of the penalty box");
-        places[currentPlayer] = places[currentPlayer] + roll;
-        if(places[currentPlayer] > 11){
-          places[currentPlayer] = places[currentPlayer] - 12;
+        /**
+         * Initializes the question set of the game
+         */
+        function initQuestions() {
+            for (var categoryId = 0, categoriesCount = categories.length; categoryId < categoriesCount; categoryId++) {
+                questions[categoryId] = [];
+                for (var i = 0; i < QUESTIONS_COUNT; i++) {
+                    questions[categoryId].push(categories[categoryId] + ' Question ' + i);
+                }
+            }
         }
 
-        console.log(players[currentPlayer] + "'s new location is " + places[currentPlayer]);
-        console.log("The category is " + currentCategory());
-        askQuestion();
-      }else{
-        console.log(players[currentPlayer] + " is not getting out of the penalty box");
-        isGettingOutOfPenaltyBox = false;
-      }
-    }else{
+        /**
+         * Checks the win condition and returns true if the current
+         * player has won or false otherwise
+         *
+         * @returns {boolean}
+         */
+        function didPlayerWin() {
+            return purses[currentPlayer] == WIN_GOLD;
+        }
 
-      places[currentPlayer] = places[currentPlayer] + roll;
-      if(places[currentPlayer] > 11){
-        places[currentPlayer] = places[currentPlayer] - 12;
-      }
+        /**
+         * Returns the current category name
+         *
+         * @returns {string}
+         */
+        function currentCategory() {
+            return categories[getCurrentCategoryId()];
+        };
 
-      console.log(players[currentPlayer] + "'s new location is " + places[currentPlayer]);
-      console.log("The category is " + currentCategory());
-      askQuestion();
+        /**
+         * Returns the category id corresponding to the current player's position
+         *
+         * @returns {number}
+         */
+        function getCurrentCategoryId() {
+            return places[currentPlayer] % categories.length;
+        }
+
+        /**
+         * Retrieves the next question from the game's question set.
+         */
+        function askNextQuestion() {
+            logMessage(questions[getCurrentCategoryId()].shift());
+        }
+
+        /**
+         * Moves the location of the current player by diceValue positions.
+         *
+         * @param diceValue
+         */
+        function moveCurrentPlayer(diceValue) {
+            places[currentPlayer] = (places[currentPlayer] + diceValue) % PLACES_COUNT;
+        }
+
+        /**
+         * Gives the current player 1 gold coin
+         */
+        function rewardCurrentPlayer() {
+            purses[currentPlayer] += 1;
+            logMessage(players[currentPlayer] + ' now has ' +
+                purses[currentPlayer] + ' Gold Coins.');
+        }
+
+        /**
+         * Initializes the state data for a new player
+         *
+         * @param playerName
+         */
+        function initNewPlayer(playerName) {
+            players.push(playerName);
+            places.push(0);
+            purses.push(0);
+            inPenaltyBox.push(false);
+        }
+
+        /**
+         * Logs received message
+         *
+         * @param message
+         */
+        function logMessage(message) {
+            console.log(message);
+        }
+
+        /**
+         * Checks if the prerequisites for playing a game are met
+         *
+         * @returns {boolean}
+         */
+        this.isPlayable = function () {
+            return playersCount >= MIN_PLAYERS;
+        }
+
+        /**
+         * Adds a new player to the game and returns true
+         * if the operation is successful or false otherwise
+         *
+         * @param playerName
+         * @returns {boolean}
+         */
+        this.addPlayer = function (playerName) {
+            if (playersCount >= MAX_PLAYERS) {
+                logMessage('Cannot add player ' + playerName + '. Maximum number of players reached.');
+                return false;
+            }
+
+            initNewPlayer(playerName);
+            playersCount++;
+
+            logMessage(playerName + ' was added');
+            logMessage((playersCount == 1) ? 'There is 1 player' : ('There are ' + playersCount + ' players'));
+
+            return true;
+        }
+
+        /**
+         * Passes the turn to the next player
+         */
+        this.nextPlayer = function() {
+            currentPlayer = (currentPlayer + 1) % playersCount;
+        }
+
+        /**
+         * Performs all actions associated with a dice roll
+         *
+         * @param diceValue
+         */
+        this.rollDice = function (diceValue) {
+            if (!this.isPlayable()) {
+                logMessage('Cannot start game. Not enough players.');
+                return;
+            }
+
+            logMessage(players[currentPlayer] + ' is the current player');
+            logMessage('He/She has rolled a ' + diceValue);
+
+            isGettingOutOfPenaltyBox = (diceValue % 2) != 0;
+            if (inPenaltyBox[currentPlayer] && !isGettingOutOfPenaltyBox) {
+                logMessage(players[currentPlayer] + ' is not getting out of the penalty box');
+                return;
+            }
+
+            if (inPenaltyBox[currentPlayer] && isGettingOutOfPenaltyBox) {
+                logMessage(players[currentPlayer] + ' is getting out of the penalty box');
+            }
+
+            moveCurrentPlayer(diceValue);
+            logMessage(players[currentPlayer] + '\'s new location is ' + places[currentPlayer]);
+
+            logMessage('The category is ' + currentCategory());
+            askNextQuestion();
+        }
+
+        /**
+         * Performs the required actions for a correct answer
+         *
+         * @returns {boolean}
+         */
+        this.wasCorrectlyAnswered = function () {
+            if (inPenaltyBox[currentPlayer] && !isGettingOutOfPenaltyBox) {
+                return false;
+            }
+
+            logMessage('Answer was correct!!!!');
+            rewardCurrentPlayer();
+
+            return didPlayerWin();
+        }
+
+        /**
+         * Performs the required actions for a wrong answer
+         *
+         * @returns {boolean}
+         */
+        this.wrongAnswer = function () {
+            logMessage('Question was incorrectly answered');
+            logMessage(players[currentPlayer] + ' was sent to the penalty box');
+            inPenaltyBox[currentPlayer] = true;
+
+            return false;
+        }
     }
-  };
 
-  this.wasCorrectlyAnswered = function(){
-    if(inPenaltyBox[currentPlayer]){
-      if(isGettingOutOfPenaltyBox){
-        console.log('Answer was correct!!!!');
-        purses[currentPlayer] += 1;
-        console.log(players[currentPlayer] + " now has " +
-                    purses[currentPlayer]  + " Gold Coins.");
+    var gameEnded = false;
+    var game = new Game();
 
-        var winner = didPlayerWin();
-        currentPlayer += 1;
-        if(currentPlayer == players.length)
-          currentPlayer = 0;
+    game.addPlayer('Chet');
+    game.addPlayer('Pat');
+    game.addPlayer('Sue');
 
-        return winner;
-      }else{
-        currentPlayer += 1;
-        if(currentPlayer == players.length)
-          currentPlayer = 0;
-        return true;
-      }
+    do {
+        game.rollDice(Math.floor(Math.random() * 6) + 1);
 
+        gameEnded = (Math.floor(Math.random() * 10) == 7) ? game.wrongAnswer() : game.wasCorrectlyAnswered();
 
-
-    }else{
-
-      console.log("Answer was correct!!!!");
-
-      purses[currentPlayer] += 1;
-      console.log(players[currentPlayer] + " now has " +
-                  purses[currentPlayer]  + " Gold Coins.");
-
-      var winner = didPlayerWin();
-
-      currentPlayer += 1;
-      if(currentPlayer == players.length)
-        currentPlayer = 0;
-
-      return winner;
-    }
-  };
-
-  this.wrongAnswer = function(){
-		console.log('Question was incorrectly answered');
-		console.log(players[currentPlayer] + " was sent to the penalty box");
-		inPenaltyBox[currentPlayer] = true;
-
-    currentPlayer += 1;
-    if(currentPlayer == players.length)
-      currentPlayer = 0;
-		return true;
-  };
-};
-
-var notAWinner = false;
-
-var game = new Game();
-
-game.add('Chet');
-game.add('Pat');
-game.add('Sue');
-
-do{
-
-  game.roll(Math.floor(Math.random()*6) + 1);
-
-  if(Math.floor(Math.random()*10) == 7){
-    notAWinner = game.wrongAnswer();
-  }else{
-    notAWinner = game.wasCorrectlyAnswered();
-  }
-
-}while(notAWinner);
+        if (!gameEnded) game.nextPlayer();
+    } while (!gameEnded);
+})();
